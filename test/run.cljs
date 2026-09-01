4218b344a4e5a64788aaad8473e4c0e95b9a2fe1
Jun Kawasaki <jun784@gmail.com>
2026-09-01 13:04:44 +0900

    otent: one derived vintage (temporal-coverage) task over KartaView open street metadata

--- test/run.cljs
+++ test/run.cljs
@@ -44,6 +44,7 @@
             [otent.parses-test]
             [otent.receipt-test]
             [otent.sanctions-test]
+            [otent.street-coverage-test]
             [otent.watchlist-test]))
 
 (def declared
@@ -60,6 +61,7 @@
     otent.parses-test
     otent.receipt-test
     otent.sanctions-test
+    otent.street-coverage-test
     otent.watchlist-test])
 
 (def min-tests 20)
